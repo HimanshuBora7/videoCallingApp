3a0db58ec55e42280b4c29095e0f42f6163a0f4c
himanshubora7 <himanshubora972@gmail.com>
2026-05-28 16:47:59 +0530

    call connecting

--- client/src/screen/room.jsx
+++ client/src/screen/room.jsx
@@ -1,51 +1,110 @@
-import React, {
-  useEffect,
-  useCallback,
-  usesState,
-  useState,
-  useRef,
-} from "react";
+import React, { useEffect, useCallback, useState, useRef } from "react";
 import { useSocket } from "../context/SocketProvider";
-import ReactPlayer from "react-player";
+import peer from "../service/peer";
 const RoomPage = () => {
   const socket = useSocket();
   const [remoteSocketId, setRemoteSocketId] = useState(null);
   const [myStream, setMyStream] = useState(null);
+  const [remoteStream, setRemoteStream] = useState(null);
+  const myVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`Email ${email} joined the room`);
     setRemoteSocketId(id);
-  });
+  }, []);
   const handleCallUser = useCallback(async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true,
       });
-      console.log("Stream obtained!", stream); // <-- CHECK YOUR CONSOLE
       setMyStream(stream);
+      console.log("Stream obtained!", stream);
+
+      for (const track of stream.getTracks()) {
+        peer.peer.addTrack(track, stream);
+      }
+
+      const offer = await peer.getOffer();
+      socket.emit("user:call", { to: remoteSocketId, offer });
     } catch (err) {
       console.error("Failed to get camera!", err);
     }
+  }, [remoteSocketId, socket]);
+
+  const handleIncomingCall = useCallback(
+    async ({ from, offer }) => {
+      setRemoteSocketId(from);
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: false,
+      });
+      setMyStream(stream);
+
+      for (const track of stream.getTracks()) {
+        peer.peer.addTrack(track, stream);
+      }
+
+      console.log("incoming call ", from, offer);
+      const ans = await peer.getAnswer(offer);
+      socket.emit("call:accepted", { to: from, ans });
+    },
+    [socket],
+  );
+  const handleCallAccepted = useCallback(({ from, ans }) => {
+    peer.setLocalDescription(ans);
+    console.log("call Accepted");
   }, []);
   useEffect(() => {
     socket.on("user:joined", handleUserJoined);
+    socket.on("incoming:call", handleIncomingCall);
+    socket.on("call:accepted", handleCallAccepted);
     return () => {
       socket.off("user:joined", handleUserJoined);
+      socket.off("incoming:call", handleIncomingCall);
+      socket.off("call:accepted", handleCallAccepted);
     };
-  }, [socket, handleUserJoined]);
+  }, [socket, handleUserJoined, handleIncomingCall, handleCallAccepted]);
+  useEffect(() => {
+    if (myVideoRef.current && myStream) {
+      myVideoRef.current.srcObject = myStream;
+    }
+  }, [myStream]);
+
+  useEffect(() => {
+    if (remoteVideoRef.current && remoteStream) {
+      remoteVideoRef.current.srcObject = remoteStream;
+    }
+  }, [remoteStream]);
+
+  useEffect(() => {
+    const handleTrack = async (ev) => {
+      const remoteStream = ev.streams;
+      console.log("GOT TRACKS!!");
+      setRemoteStream(remoteStream[0]);
+    };
+    peer.peer.addEventListener("track", handleTrack);
+    return () => {
+      peer.peer.removeEventListener("track", handleTrack);
+    };
+  }, []);
+
   return (
     <div>
       <h1>Room page</h1>
       <h4>{remoteSocketId ? "Connected" : "No one in the room"}</h4>
       {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}
       {myStream && (
-        <ReactPlayer
-          playing
-          muted
-          height="300px"
-          width="600px"
-          url={myStream}
-        />
+        <>
+          <h1>My Stream</h1>
+          <video ref={myVideoRef} autoPlay muted height="100px" width="200px" />
+        </>
+      )}
+      {remoteStream && (
+        <>
+          <h1>Remote Stream</h1>
+          <video ref={remoteVideoRef} autoPlay height="300px" width="600px" />
+        </>
       )}
     </div>
   );
